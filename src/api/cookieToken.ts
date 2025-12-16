c3aa1ee0a87d9026dbc63180e6b49af2ff3a8041
Кирилл <122471811+makorella2238@users.noreply.github.com>
2025-12-16 16:08:08 +0300

    Update cookieToken.ts

--- src/api/cookieToken.ts
+++ src/api/cookieToken.ts
@@ -11,6 +11,4 @@ export const getTokenFromCookies = (): string | undefined => {
     }
 
     console.warn('[AUTH] Token отсутствует в cookie',);
-
-    // return 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiMmJoZWxwLnJ1IiwiaWF0IjoxNzY1MTkyNDgwLCJuYmYiOjE3NjUxOTI0ODAsImV4cCI6MTc2NTc5NzI4MCwidXNlcl9pZCI6NTg4N30.BsAL8Fs5rOh9doToq5NcPglwcR1qnWFEfAq8gp4pGCM'
 };
